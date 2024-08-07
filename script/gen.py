fix bug for possible overflow in update_width

script/gen.py
@@ -6,5 +6,13 @@ def fft(n, m):
             ret[i,j] = np.exp(1j*i*j*2*np.pi/n)*(2**m)
     return np.round(ret.real), np.round(ret.imag)
 
-print(fft(5, 6))
-print(fft(9, 5)) 
+# print(fft(5, 6))
+# print(fft(9, 5)) 
+
+n = 15
+print(n, n)
+matrix = fft(n, n)
+for i in range(n):
+    for j in range(n):
+        print(int(matrix[0][i,j]), end=' ')
+    print()